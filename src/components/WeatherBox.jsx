dc1e29ebdebd2845ae9b01d7569535782d5576a3
JiHyoung You <fbwlgud51@gmail.com>
2025-10-25 04:08:18 +0900

    style: 전체 스타일 적용

--- src/components/WeatherBox.jsx
+++ src/components/WeatherBox.jsx
@@ -1,16 +1,28 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
 
 const WeatherBox = ({ weather }) => {
   console.log(weather, weather?.name);
   return (
-    <div>
-      <Typography>{weather?.name}</Typography>
+    <Container
+      sx={{
+        mb: 2,
+        p: 4,
+        backgroundColor: "rgba(255, 255, 255, 0.3)",
+        backdropFilter: "blur(5px)",
+        boxShadow:
+          "4px 4px 8px rgba(0, 0, 0, 0.1), -4px -4px 8px rgba(255, 255, 255, 0.5)",
+
+        color: "black",
+        borderRadius: 4,
+      }}
+    >
+      <Typography variant="h4">{weather?.name}</Typography>
       <Typography>
         {Math.floor(weather?.main.temp - 273.15)}℃ / {weather?.main.temp}℉
       </Typography>
       <Typography>{weather?.weather[0].description}</Typography>
-    </div>
+    </Container>
   );
 };
 
--- src/components/WeatherButtons.jsx
+++ src/components/WeatherButtons.jsx
@@ -3,7 +3,28 @@ import { ButtonGroup, Button } from "@mui/material";
 
 const WeatherButtons = () => {
   return (
-    <ButtonGroup variant="contained" aria-label="Basic button group">
+    <ButtonGroup
+      variant="contained"
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        boxShadow:
+          "4px 4px 8px rgba(0, 0, 0, 0.1), -4px -4px 8px rgba(255, 255, 255, 0.5)",
+        "& .MuiButton-root": {
+          backgroundColor: "rgba(255, 255, 255, 0.3)",
+          backdropFilter: "blur(3px)",
+          color: "black",
+          border: "none",
+          outline: "none",
+          "&:hover": {
+            color: "white",
+            fontWeight: "bold",
+            backgroundColor: "rgba(33, 162, 198, 0.39)",
+          },
+        },
+      }}
+    >
       <Button>Current Location</Button>
       <Button>Seoul</Button>
       <Button>Tokyo</Button>
